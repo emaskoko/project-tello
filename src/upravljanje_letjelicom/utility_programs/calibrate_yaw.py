0b94ca833119f990acf2b78dc035c22e873e671d
Patrik Landeka <patrik.landeka@fer.unizg.hr>
2024-12-31 16:02:23 +0100

    Update calibrate_yaw.py

--- src/upravljanje_letjelicom/utility_programs/calibrate_yaw.py
+++ src/upravljanje_letjelicom/utility_programs/calibrate_yaw.py
@@ -1,15 +1,10 @@
 import sys
 sys.path.append('../')
 from TelloClass import TelloClass
-from time import sleep
 
-if __name__ == '__main__':
-    try:
-        tello_class = TelloClass()
-        print(f'Initial yaw: {tello_class._tello.get_yaw()}')
-    except KeyboardInterrupt:
-        print('Aborting...\nDrone is landing...')
-    finally:
-        pass
+#orientate drone 
+tello_class = TelloClass()
+print(f'Initial yaw: {tello_class._tello.get_yaw()}')
+    
     
     
