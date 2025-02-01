7596910119f9da6b8ddba38b8ccf92376f2dc8a4
PetarKrtalic <petar.krtalic@fer.unizg.hr>
2025-02-01 20:21:05 +0100

    Dodano detektiranje paketa

---
+++ src/obrada_slike/YOLO_model.py
@@ -0,0 +1,32 @@
+# Import the InferencePipeline object
+from inference import InferencePipeline
+import cv2
+from PIL import Image 
+import PIL
+
+def my_sink(result, video_frame):
+    if result.get("output_image"):  # Provera da li postoji slika u rezultatu
+        output_image = result["output_image"].numpy_image  # Dobijamo numpy array
+
+        if output_image is not None:
+            # Prikaz slike
+            cv2.imshow("Workflow Image", output_image)
+            cv2.waitKey(1)
+
+            # Čuvanje slike u output.jpg
+            cv2.imwrite("output.jpg", output_image)
+
+    print(result)  # Prikaz rezultat
+    
+
+# initialize a pipeline object
+pipeline = InferencePipeline.init_with_workflow(
+    api_key="68uomVMA1kKCZEGjK0y4",
+    workspace_name="projekt-r",
+    workflow_id="detect-count-and-visualize-3",
+    video_reference="vid5.mp4", # Path to video, device id (int, usually 0 for built in webcams), or RTSP stream url
+    max_fps=30,
+    on_prediction=my_sink
+)
+pipeline.start() #start the pipeline
+pipeline.join() #wait for the pipeline thread to finish
