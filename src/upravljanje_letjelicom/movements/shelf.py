5be8bd67ab522a338d736663825b5b80ac685e28
Patrik Landeka <patrik.landeka@fer.unizg.hr>
2025-02-05 00:51:27 +0100

    Update shelf.py

--- src/upravljanje_letjelicom/movements/shelf.py
+++ src/upravljanje_letjelicom/movements/shelf.py
@@ -1,37 +1,45 @@
 import sys
-sys.path.append('../')
-from upravljanje_letjelicom.TelloClass import TelloClass
+from TelloClass import TelloClass
 from time import sleep
 
-DISTANCE = 150      #distance from shelf in [cm]
-WIDTH = 300         #shelf width in [cm]
-HEIGHT = 200        #shelf height in [cm]
-COL_WIDTH = 50      #width of one column in [cm]
-GAP = 60            #how much space will be between drone and the shelf in [cm]
-MARGIN = 40         #how much space will be left where drone won't fly  
-ORIENTATION = -4    #initial yaw where drone must be facing towards in °
 
-if __name__ == '__main__':
+DISTANCE = 100      #distance from shelf in [cm]
+
+COL_WIDTH = 40      #width of one column in [cm]
+GAP = 0            #how much space will be between drone and the shelf in [cm]
+MARGIN = 0         #how much space will be left where drone won't fly  
+ORIENTATION = 171    #initial yaw where drone must be facing towards in °
+
+tello_class = TelloClass()
+
+def shelf_scan(width=0, height=0):
+    #shelf width in [cm]
+    #shelf height in [cm]
+    print(f'width = {width} height = {height}')
     try:
-        tello_class = TelloClass()
-        tello_class.c_takeoff(orientation=ORIENTATION)
-        lower_dist = tello_class._tello.get_height() - MARGIN
-        if(lower_dist > 20):
-            tello_class._tello.move_down(lower_dist)
-            sleep(2)
+        #tello_class = TelloClass()
+        tello_class.c_takeoff(video_name='proba_video.avi', path='./')
+        sleep(2)
+        tello_class._tello.land()
+        # return
+        # lower_dist = tello_class._tello.get_height() - MARGIN
+        #if(lower_dist > 20):
+            #tello_class._tello.move_down(lower_dist)
+            #sleep(2)
         tello_class._tello.move_forward(DISTANCE - GAP)
-        for i in range(WIDTH // COL_WIDTH):
+        for i in range(width // COL_WIDTH):
             sleep(2)
             if(i % 2 == 0):
-                tello_class._tello.move_up(HEIGHT - 2 * MARGIN)
+                tello_class._tello.move_up(height - 2 * MARGIN)
             else:
-                tello_class._tello.move_down(HEIGHT - 2 * MARGIN)
-            sleep(2)
+                tello_class._tello.move_down(height - 2 * MARGIN)
+            sleep(5)
             tello_class._tello.move_right(COL_WIDTH)
-
-        tello_class.detect_packages(model_path='yolov5s.pt')    
     except KeyboardInterrupt:
         print('Aborting...\nDrone is landing...')
     finally:
         tello_class._tello.land()
         tello_class.draw_graph()
+
+if __name__ == '__main__':
+    shelf_scan()
